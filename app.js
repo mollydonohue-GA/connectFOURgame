eae3ca8f5dbe126d03c5bcc26533168e6f33c908
Molly Donohue <mollykennan@gmail.com>
2016-01-23 19:11:40 -0500

    can now find row wins

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 console.log("running");
+
+var board = [
+ ['W', 'W', 'W', 'W', 'W', 'W', 'W'],
+ ['W', 'W', 'W', 'W', 'W', 'W', 'W'],
+ ['W', 'W', 'W', 'W', 'W', 'W', 'W'],
+ ['W', 'W', 'W', 'W', 'W', 'W', 'W'],
+ ['W', 'W', 'W', 'W', 'W', 'W', 'W'],
+ ['W', 'W', 'W', 'W', 'W', 'W', 'W']];
+
+
+var checkWin = function() {
+	for (var i = 0; i < board.length; i++) {
+	
+		if (board[i].toString().search("B,B,B,B") >= 0){
+			alert("Black wins!");
+			location.reload();
+		} else if (board[i].toString().search("Y,Y,Y,Y") >= 0) {
+			alert("Yellow wins!");
+			location.reload();
+		}else{
+			console.log("keep playing");
+		}	
+	};	
+
+};
+// .toString
+//string.search(searchvalue) 
+
+
 $(document).ready(function() {
 
-var turnCount = 0;
+var turn = true;
 
-var turn = true; 
+//board[0][0] = 'y'
+
 
 var spot = $(".spot");
 console.log(spot);
@@ -12,63 +42,36 @@ var buttonReset = $(".reset");
 console.log(buttonReset);
 
 var spotClicked = function(){
+	var colNum = parseInt($(this).parent().attr('id').substr(3, 1));
+//colNum - grabbing the col number
+//substr pulling the number from the string
+//parseInt makes the string a number
+//updating the board var 	
 	
-	var spotCheck = $(this).parent().children();
+	var clickedColumnSpots = $(this).parent().children();
 
-	for (var i = spotCheck.length - 1; i >= 0; i--) {
-		if (!($(spotCheck[i]).hasClass('clicked'))){
-
+	for (var i = clickedColumnSpots.length - 1; i >= 0; i--) {
+		if (!($(clickedColumnSpots[i]).hasClass('clicked'))){
+			turn = !turn;
+			$(clickedColumnSpots[i]).addClass('clicked');
 			if (turn) {
-				$(spotCheck[i]).css('background-color', 'yellow');
-				$(spotCheck[i]).addClass('clicked'); 
-				$(spotCheck[i]).addClass('yellow');
-				turn = false;
-				turnCount ++;
-			} else {
-				$(spotCheck[i]).css('background-color', 'black');
-				$(spotCheck[i]).addClass('clicked'); 	
-				$(spotCheck[i]).addClass('black'); 
-				turn = true;
-				turnCount ++;
-			}
+				$(clickedColumnSpots[i]).css('background-color', 'yellow');
+				$(clickedColumnSpots[i]).addClass('yellow');
+				board[i][colNum] = 'Y'
+//i is the row 
+//this is updating the var board
 
-			// console.log("free space");
-			// console.log(spotCheck[i]);
-			
-			// $(spotCheck[i]).addClass('clicked'); 
-			// if ($(this).hasClass('.clicked')){
-			// turn = turn;
-			// // turnCount ++;
-			// }else {
-			// 	if (turn){
-			// 		$(this).css('background-color', 'yellow');
-			// 		// $(this).css('cursor', 'none');
-			// 		$(this).addClass('.clicked'); 
-			// 		$(this).addClass('.yellow');
-			// 		turn = false;
-			// 		turnCount ++;
-			// 	} else {
-			// 		$(this).css('background-color', 'black');
-			// 		// $(this).css('cursor', 'none');
-			// 		$(this).addClass('.clicked'); 	
-			// 		$(this).addClass('.black');
-			// 		turn = true;
-			// 		turnCount ++;
-			// 		console.log(turnCount);
-			// 	} 
-			// }
+			} else {
+				$(clickedColumnSpots[i]).css('background-color', 'black');	
+				$(clickedColumnSpots[i]).addClass('black'); 
+				board[i][colNum] = 'B'	
+			}
+			checkWin();
 			break;	
-		} else {
-			console.log("nah");
 		}
 	};
 
-	if ($(this).hasClass('row5')){
-		($(spot).addClass('row5'));	
-		// turnCount ++;
-		
-				
-	} 
+
 	// if ($(this).hasClass('row2')){
 	// alert("yellow wins!");
 	// ($(spot).css('background-color', 'yellow'));
